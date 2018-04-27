e5aaf67da682609b5e395c997182d9bef526dec4
Bernard Cooley <bernardcooley@Bernards-MBP.lan>
2018-04-28 00:13:19 +0100

    landing page fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NavComponent } from './components/nav/nav.component';
 
-
+//Components
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
@@ -10,33 +11,35 @@ import { VideosPageComponent } from './pages/videos-page/videos-page.component';
 import { MusicPageComponent } from './pages/music-page/music-page.component';
 import { GigsPageComponent } from './pages/gigs-page/gigs-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { BookingFormComponent } from './components/booking-form/booking-form.component';
+import { SocialLinksComponent } from './components/social-links/social-links.component';
+import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
+import { PageDividerComponent } from './components/page-divider/page-divider.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MainSiteComponent } from './pages/main-site/main-site.component';
+
+//Modules
+import { RouterModule, Routes } from '@angular/router';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewsletterComponent } from './components/newsletter/newsletter.component';
-import { BookingFormComponent } from './components/booking-form/booking-form.component';
-import { SocialLinksComponent } from './components/social-links/social-links.component';
-import { ContactService } from './services/contact/contact.service';
-import { environment } from '../environments/environment';
 import { HttpModule } from '@angular/http';
-
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { NavComponent } from './components/nav/nav.component';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
-import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
-import { PageDividerComponent } from './components/page-divider/page-divider.component';
-import { SafePipe } from './pipes/safe-pipe.pipe';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
-import { CarouselComponent } from './components/carousel/carousel.component';
 import { Ng4FittextModule } from 'ng4-fittext';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { MainSiteComponent } from './pages/main-site/main-site.component';
 
+//Services
+import { ContactService } from './services/contact/contact.service';
+
+import { environment } from '../environments/environment';
+import { SafePipe } from './pipes/safe-pipe.pipe';
 export const firestoreConfig = environment.firestore;
 
 
